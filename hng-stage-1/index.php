55500b180c6dce4f8c28f53b73df0af03a8f2f86
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2018-04-10 20:48:25 +0100

    add first page with time displayed

---
+++ hng-stage-1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html class="bg">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HNG Welcome Page</title>
+  <link rel="stylesheet" type="text/css" href="./assets/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open
+		+Sans|Kaushan+Script">
+</head>
+
+<body>
+  <div class="center">
+    <h1 class="hey">HEY THERE!</h1>
+    <h1 class="welcome">WELCOME HOME</h1>
+    <div class="card card1">
+      <div class="card card2">
+        <h1 class="time">
+          <?php
+            echo date('H:i:s');
+          ?>
+        </h1>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
